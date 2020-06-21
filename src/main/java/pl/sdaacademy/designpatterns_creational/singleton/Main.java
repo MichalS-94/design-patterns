f2178ef549f6f394af987e974d00b1ddc05d418d
Użytkownik <uzytkownik@kursGIT.pl>
2020-06-21 12:31:53 +0200

    added singleton

---
+++ src/main/java/pl/sdaacademy/designpatterns_creational/singleton/Main.java
@@ -0,0 +1,16 @@
+package pl.sdaacademy.designpatterns_creational.singleton;
+
+import java.time.LocalDate;
+
+public class Main{
+    public static void main(String[] args) {
+        LocalDate d;
+        UniverseV1 universe1 = UniverseV1.INSTANCE;
+        UniverseV2 universe2 = UniverseV2.getInstance();
+        UniverseV3 universe3 = UniverseV3.getInstance();
+        UniverseV4 universe4 = UniverseV4.getInstance();
+
+        System.out.println(universe3 == UniverseV3.getInstance());
+        System.out.println(universe4 == UniverseV4.getInstance());
+    }
+}
---
+++ src/main/java/pl/sdaacademy/designpatterns_creational/singleton/UniverseV2.java
@@ -0,0 +1,31 @@
+package pl.sdaacademy.designpatterns_creational.singleton;
+
+public class UniverseV2 {
+    private static UniverseV2 instance = new UniverseV2();
+
+    private double mass;
+    private double avgTemperature;
+
+
+    public static UniverseV2 getInstance() {
+        return instance;
+    }
+
+    private UniverseV2() {}
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getAvgTemperature() {
+        return avgTemperature;
+    }
+
+    public void setAvgTemperature(double avgTemperature) {
+        this.avgTemperature = avgTemperature;
+    }
+}
